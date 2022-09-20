e5a79cb0c2e6363ba2c5a44d60466313c0d13e26
Neuroquila-n8fall <david.ginsbach@googlemail.com>
2022-09-20 10:29:46 +0200

    Update Config Template.
    Revert changes to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -324,12 +324,6 @@ void loop()
     }
   }
 
-  runEverySeconds(30)
-  {
-    CANMessage msg = PrepareMessage(0xF9);
-    SendMessage(msg);
-  }
-
   //——————————————————————————————————————————————————————————————————————————————
   // Actions performed every five seconds
   //——————————————————————————————————————————————————————————————————————————————
