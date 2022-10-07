34aeaa866ba97a863a807c24ecc4c49bc9f453d2
Neuroquila-n8fall <david.ginsbach@googlemail.com>
2022-10-07 08:39:30 +0200

    Delete unused function

--- src/webconfig.cpp
+++ src/webconfig.cpp
@@ -927,16 +927,3 @@ void getSystemStatus(AsyncWebServerRequest *request)
 
     sendJson(doc, request);
 }
-
-// Make size of files human readable
-String humanReadableSize(const size_t bytes)
-{
-    if (bytes < 1024)
-        return String(bytes) + " B";
-    else if (bytes < (1024 * 1024))
-        return String(bytes / 1024.0) + " KB";
-    else if (bytes < (1024 * 1024 * 1024))
-        return String(bytes / 1024.0 / 1024.0) + " MB";
-    else
-        return String(bytes / 1024.0 / 1024.0 / 1024.0) + " GB";
-}
